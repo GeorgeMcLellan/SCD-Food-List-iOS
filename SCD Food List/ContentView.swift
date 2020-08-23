84aae35f0afa88520124625a8302ea357059ec4d
George <georgemclellan0@gmail.com>
2020-08-23 16:58:14 +1000

    Initial Commit

---
+++ SCD Food List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SCD Food List
+//
+//  Created by George McLellan on 23/8/20.
+//  Copyright © 2020 George McLellan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
